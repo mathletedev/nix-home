fe21c5a2d881c4403d6a4b269f399b40c032002b
Neal Wang <nealwang.sh@protonmail.com>
2023-07-03 14:12:20 -0700

    nvim-cmp snippet jumping

--- src/nvim/lua/servers.lua
+++ src/nvim/lua/servers.lua
@@ -13,6 +13,8 @@ cmp.setup {
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
 			else
 				fallback()
 			end
@@ -20,6 +22,8 @@ cmp.setup {
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
