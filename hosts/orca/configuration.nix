5398d21d223243aef99c73d77ef792df02fd2aad
Neal Wang <nealwang.sh@protonmail.com>
2023-11-17 09:22:02 -0800

    lots of changes

--- hosts/orca/configuration.nix
+++ hosts/orca/configuration.nix
@@ -1,12 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
-    <nixos-hardware/microsoft/surface/common>
     ./hardware-configuration.nix
+    # <nixos-hardware/microsoft/surface/common>
   ];
-
-  microsoft-surface.kernelVersion = "6.1.18";
+  # microsoft-surface.kernelVersion = "6.1.18";
 
   boot = {
     loader = {
@@ -14,6 +13,8 @@
       systemd-boot.enable = true;
     };
   };
+
+  nix.autoOptimiseStore = true;
 
   networking = {
     firewall.enable = true;
@@ -29,9 +30,28 @@
     useXkbConfig = true;
   };
 
+  users = {
+    defaultUserShell = pkgs.fish;
+    users.neo = {
+      extraGroups = [ "wheel" "networkmanager" "audio" "adbusers" ];
+      isNormalUser = true;
+    };
+  };
+
   services = {
     blueman.enable = true;
     logind.lidSwitch = "suspend";
+    postgresql = {
+      enable = true;
+      authentication = lib.mkForce ''
+        # Generated file; do not edit!
+        # TYPE  DATABASE        USER            ADDRESS                 METHOD
+        local   all             all                                     trust
+        host    all             all             127.0.0.1/32            trust
+        host    all             all             ::1/128                 trust
+      '';
+    };
+    udev.packages = with pkgs; [ android-udev-rules ];
     udisks2.enable = true;
     upower.enable = true;
     xserver = {
@@ -44,33 +64,41 @@
           tapping = true;
         };
       };
+      gdk-pixbuf.modulePackages = with pkgs; [ librsvg ];
       windowManager.xmonad.enable = true;
       xkbOptions = "caps:escape";
     };
   };
 
+  gtk.iconCache.enable = true;
+
   sound.enable = true;
 
   hardware = {
-    bluetooth.enable = true;
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
     opengl.driSupport32Bit = true;
     pulseaudio.enable = true;
-  };
-
-  users = {
-    defaultUserShell = pkgs.fish;
-    users.neo = {
-      extraGroups = [ "wheel" "networkmanager" "audio" ];
-      isNormalUser = true;
-    };
   };
 
   environment.systemPackages = with pkgs; [ git neovim ];
 
   programs = {
+    adb.enable = true;
+    dconf.enable = true;
     fish.enable = true;
     slock.enable = true;
   };
 
-  system.stateVersion = "23.05";
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    inputMethod = {
+      enabled = "ibus";
+      ibus.engines = with pkgs.ibus-engines; [ libpinyin ];
+    };
+  };
+
+  system.stateVersion = "23.11";
 }
