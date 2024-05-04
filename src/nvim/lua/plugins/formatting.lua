25a8ac8b72bdffcb5414e624541637239e981b76
Neal Wang <nealwang.sh@protonmail.com>
2024-05-03 20:48:38 -0700

    gdformat

--- src/nvim/lua/plugins/formatting.lua
+++ src/nvim/lua/plugins/formatting.lua
@@ -3,30 +3,30 @@ return {
 		"stevearc/conform.nvim",
 		opts = {
 			formatters_by_ft = {
+				astro = { "prettierd" },
+				css = { "prettierd" },
+				gdscript = { "gdformat" },
 				go = { "gofmt" },
+				javascript = { "prettierd" },
+				json = { "prettierd" },
 				lua = { "stylua" },
 				python = { "black" },
 				rust = { "rustfmt" },
+				svelte = { "prettierd" },
+				typescript = { "prettierd" },
+				yaml = { "prettierd" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
 				timeout_ms = 500,
 			},
+			formatters = {
+				gdformat = {
+					command = "gdformat",
+					args = "$FILENAME",
+					stdin = false,
+				},
+			},
 		},
-	},
-	{
-		"nvimdev/guard.nvim",
-		dependencies = {
-			"nvimdev/guard-collection",
-		},
-		config = function()
-			local ft = require "guard.filetype"
-			ft("typescript,javascript,json,css,yaml,astro,svelte"):fmt "prettier"
-
-			require("guard").setup {
-				fmt_on_save = true,
-				lsp_as_default_formatter = true,
-			}
-		end,
 	},
 }
