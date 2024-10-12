f2b10c718007eb2c1fdd9926bc3cad2c9b8ab40b
Neal Wang <nealwang.sh@protonmail.com>
2024-10-11 19:24:59 -0700

    feat: harpoon remove telescope ui

--- src/nvim/lua/plugins/harpoon.lua
+++ src/nvim/lua/plugins/harpoon.lua
@@ -4,38 +4,27 @@ return {
 		branch = "harpoon2",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
 		},
 		config = function()
 			local harpoon = require "harpoon"
-			harpoon.setup {}
+			harpoon:setup {}
 
 			vim.keymap.set("n", "<Leader>m", function()
 				harpoon:list():add()
 			end)
-
-			local conf = require("telescope.config").values
-			local function toggle_telescope(harpoon_files)
-				local file_paths = {}
-				for _, item in ipairs(harpoon_files.items) do
-					table.insert(file_paths, item.value)
-				end
-
-				require("telescope.pickers")
-					.new({}, {
-						prompt_title = "Harpoon",
-						finder = require("telescope.finders").new_table {
-							results = file_paths,
-						},
-						previewer = conf.file_previewer {},
-						sorter = conf.generic_sorter {},
-					})
-					:find()
-			end
-
 			vim.keymap.set("n", "<Leader>p", function()
-				toggle_telescope(harpoon:list())
+				harpoon.ui:toggle_quick_menu(harpoon:list(), {
+					border = "rounded",
+					title_pos = "center",
+					ui_width_ratio = 0.40,
+				})
 			end)
+
+			for i = 0, 9 do
+				vim.keymap.set("n", "<Leader>" .. i, function()
+					harpoon:list():select(i)
+				end)
+			end
 		end,
 	},
 }
