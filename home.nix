3ddf47b5179620b43665eea82fbfa439928c2231
Neal Wang <nealwang.sh@protonmail.com>
2025-01-13 22:41:14 -0800

    feat: custom ghci prompt

--- home.nix
+++ home.nix
@@ -27,6 +27,10 @@ in
         INSTALL_DIR=$HOME/tools/Xilinx
         VERSION=2024.1
       '';
+      ".ghc/ghci.conf".text = ''
+        :set prompt "\ESC[1;35m\x03BB> \ESC[m"
+        :set prompt-cont "\ESC[1;35m > \ESC[m"
+      '';
       ".npmrc".text = "prefix=~/.npm-packages";
     };
     homeDirectory = "/home/neo";
